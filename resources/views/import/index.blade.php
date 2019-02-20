63b4553c4527685f569bea8a460f43c447e6856a
Daniel Espinoza <daniel@growdevelopment.com>
2019-02-20 14:00:13 -0600

    Add account dropdown

--- resources/views/import/index.blade.php
+++ resources/views/import/index.blade.php
@@ -22,6 +22,15 @@
 
     <form method="POST" action="{{ route('import.upload') }}" aria-label="Import" enctype="multipart/form-data">
         @csrf
+
+        <div class="form-group row mb-0">
+            <label class="col-sm-3 control-label">{{ trans('labels.transactions.properties.account_id') }}</label>
+            <div class="col-sm-8">
+                <select class="form-control" name="account_id" required>
+                    @include('partials.dropdowns.accounts')
+                </select>
+            </div>
+        </div>
         <div class="form-group row mb-0">
             <div class="col-md-8 offset-md-4">
                 <input type="file" name="myfile" id="myfile" />
